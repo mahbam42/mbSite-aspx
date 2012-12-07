6260aa58bd492e537db73bb02fda3640ef3d8b10
mahbam42 <max.brother@gmail.com>
2012-12-06 20:11:59 -0800

    Initial Commit of mbSite2. The ASPX version.

---
+++ Default.aspx.vb
@@ -0,0 +1,18 @@
+
+Option Explicit On
+Option Strict On
+
+Imports System
+Imports System.Web
+Imports System.Web.UI
+
+
+
+Partial Public Class [Default]
+	Inherits System.Web.UI.Page
+	
+	Public Overridable Sub button1Clicked(ByVal sender As Object, ByVal args As EventArgs)
+		button1.Text = "You clicked me"
+	End Sub
+End Class
+
---
+++ Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Inherits="mbSite2.Global" %>
