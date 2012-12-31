4dfeb875503f22f6b32e20c996a14b19d1d0fc0a
Max <max.brother@gmail.com>
2012-12-30 16:24:55 -0800

    created personal about page

---
+++ about.aspx.vb
@@ -0,0 +1,9 @@
+﻿
+Partial Class about
+    Inherits System.Web.UI.Page
+
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        lblTitle.Text = "About"
+    End Sub
+End Class
